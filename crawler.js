2ed5c85cb01a0cc28274775ff52ca5b7c8343ef5
Racro <ritik.r@nyu.edu>
2024-10-10 19:02:01 -0400

    v3 -> v2

--- crawler.js
+++ crawler.js
@@ -75,7 +75,7 @@ function openBrowser(log, proxyHost, executablePath, extension) {
                 '--disable-features=IsolateOrigins,site-per-process',
                 '--start-maximized',
                 `--disable-extensions-except=./extn_src/${extension}_v2`,
-                `--load-extension=./extn_src/${extension}_v3`,
+                `--load-extension=./extn_src/${extension}_v2`,
                 // '--user-data-dir=./saved_session/',
                 '--user-data-dir=./temp_session/',
                 '--display='+xvfb._display,
